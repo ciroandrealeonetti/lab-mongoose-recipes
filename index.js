7d135546f08debb0e6c3dfbe808c283c8e24fef2
Andrea Leonetti <ciroandrealeonetti@gmail.com>
2022-10-27 16:58:38 +0100

    solved lab

--- index.js
+++ index.js
@@ -18,11 +18,39 @@ const manageRecipes = async () => {
     // Before adding any recipes to the database, let's remove all existing ones
     await Recipe.deleteMany();
 
+    const bolognese = await Recipe.create({
+
+      title: "Pasta Bolognese",
+      level: "Easy Peasy",
+      ingredients: ["Meat", "Tomato sauce", "Pasta", "Parmigiano"],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image: "https://media-assets.lacucinaitaliana.it/photos/61fb130ae334e788eab040b9/1:1/w_2240,c_limit/Tagliatelle-con-rag%C3%B9-alla-bolognese.jpg",
+      duration: 120,
+      creator: "Your Nonna",
+      created: "810-10-12",
+
+    });
+console.log(bolognese.title);
+
+await Recipe.insertMany(data);
+
+data.forEach(element => console.log(element.title));
+
+let updateRecipe = await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true});
+console.log(updateRecipe);
+  
+let deleteRecipe = await Recipe.deleteOne({title: "Carrot Cake"});
+console.log(deleteRecipe)
+
+dbConnection.disconnect();
+
     // Run your code here, after you have insured that the connection was made
   } catch (error) {
     console.log(error);
   }
 };
+
 
 manageRecipes();
 
